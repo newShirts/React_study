dea43f5491604a9aeffea05730d50a7e834a5019
newShirts <jinsol56@gmail.com>
2023-02-24 21:13:11 +0900

    0218_0219_react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//04 useState - 버튼 동적 구현하기
+function App(){
+  return (<Counter/>)
 }
 
 export default App;
+
+//03 props : 두 컴포넌트 사이의 데이터 전송
+// function App(){
+//   return (<Hello name='react' color='red'/>)
+// }
+
+// export default App;
+
+// 02 컴포넌트 export, import 연습
+// function App() {
+//   return <MyComponent/>;
+// }
+
+// export default App;
+
+
+
+// 01 - JSX 테스트 
+// function App() { /*함수형 컴포넌트*/
+
+//   const text = 'JSX & React project!'
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           {text}
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
